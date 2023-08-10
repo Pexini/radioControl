55fcd8526ad0d50e90553f46c308231cdb2e1e54
Pavel Davydov <Davydov.works@gmail.com>
2023-08-10 12:30:47 -0400

    add @test CheckSetMetod

--- src/test/java/ru/netology/radioTest.java
+++ src/test/java/ru/netology/radioTest.java
@@ -108,4 +108,60 @@ class RadioTest {
         assertEquals(0, radio.getCurrentVolume());
     }
 
+    @Test
+    public void setMaxParamWhenNull() {
+        Radio radio = new Radio(0,0);
+        radio.setCurrentStation(9);
+        assertEquals(9, radio.getCurrentStation());
+        radio.setCurrentVolume(99);
+        assertEquals(99, radio.getCurrentVolume());
+    }
+    @Test
+    public void setMinCurrentParam() {
+        Radio radio = new Radio(0,0);
+        radio.setCurrentStation(radio.getCurrentStation());
+        assertEquals(0, radio.getCurrentStation());
+        radio.setCurrentVolume(radio.getCurrentVolume());
+        assertEquals(0, radio.getCurrentVolume());
+    }
+    @Test
+    public void setMaxCurrentParam() {
+        Radio radio = new Radio(9,99);
+        radio.setCurrentStation(radio.getCurrentStation());
+        assertEquals(9, radio.getCurrentStation());
+        radio.setCurrentVolume(radio.getCurrentVolume());
+        assertEquals(99, radio.getCurrentVolume());
+    }
+    @Test
+    public void setMaxParamWhenMin() {
+        Radio radio = new Radio(0,0);
+        radio.setCurrentStation(9);
+        assertEquals(9, radio.getCurrentStation());
+        radio.setCurrentVolume(99);
+        assertEquals(99, radio.getCurrentVolume());
+    }
+    @Test
+    public void setInvalidStationLessThanMin() {
+        Radio radio = new Radio(0,0);
+        radio.setCurrentStation(-1);
+        assertEquals(0, radio.getCurrentStation());
+        radio.setCurrentVolume(-1);
+        assertEquals(0, radio.getCurrentVolume());
+    }
+    @Test
+    public void setInvalidStationGreaterThanMax() {
+        Radio radio = new Radio(0,0);
+        radio.setCurrentStation(+1);
+        assertEquals(1, radio.getCurrentStation());
+        radio.setCurrentVolume(+1);
+        assertEquals(1, radio.getCurrentVolume());
+    }
+    @Test
+    public void setInvalidStationGreaterMax() {
+        Radio radio = new Radio(0,0);
+        radio.setCurrentStation(10);
+        assertEquals(0, radio.getCurrentStation());
+        radio.setCurrentVolume(101);
+        assertEquals(0, radio.getCurrentVolume());
+    }
 }
