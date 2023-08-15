eff57817b89b0575f42c66f60470817407e165e3
Pavel Davydov <Davydov.works@gmail.com>
2023-08-15 04:50:42 -0400

    Add two constructors + @Test

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,12 +1,5 @@
 package ru.netology;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class Radio {
     private int minCurrentStation = 0;
     private int maxCurrentStation = 9;
@@ -15,10 +8,73 @@ public class Radio {
     private int currentStation = minCurrentStation;
     private int currentVolume = minCurrentVolume;
 
-    public Radio(int numRadio, int sizeVolume) {
-        currentStation = numRadio;
+    public Radio() {
+        this.maxCurrentStation = 9;
+    }
 
-        currentVolume = sizeVolume;
+    public Radio(int stationCount) {
+        this.maxCurrentStation = stationCount - 1;
+    }
+
+    public Radio(int numRadio, int sizeVolume) {
+        if (numRadio >= minCurrentStation && numRadio <= maxCurrentStation) {
+            setCurrentStation(numRadio);
+        } else {
+            currentStation = minCurrentStation;
+        }
+        if (sizeVolume >= minCurrentVolume && sizeVolume <= maxCurrentVolume) {
+            setCurrentVolume(sizeVolume);
+        } else {
+            currentVolume = minCurrentVolume;
+        }
+
+    }
+
+    public void setCurrentStation(int numRadio) {
+        if (numRadio >= minCurrentStation && numRadio <= maxCurrentStation) {
+            currentStation = numRadio;
+        } else if (numRadio > maxCurrentStation) {
+            currentStation = maxCurrentStation;
+        } else if (numRadio < minCurrentStation) {
+            currentStation = minCurrentStation;
+        }
+    }
+
+
+    public void setCurrentVolume(int sizeVolume) {
+        if (sizeVolume <= minCurrentVolume) {
+            sizeVolume = minCurrentVolume;
+
+        }
+        if (sizeVolume <= maxCurrentVolume) {
+            currentVolume = sizeVolume;
+        } else {
+            currentVolume = maxCurrentVolume;
+        }
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public int getMinCurrentStation() {
+        return minCurrentStation;
+    }
+
+    public int getMaxCurrentStation() {
+        return maxCurrentStation;
+    }
+
+    public int getMinCurrentVolume() {
+        return minCurrentVolume;
+    }
+
+    public int getMaxCurrentVolume() {
+        return maxCurrentVolume;
     }
 
     public void next() {
@@ -52,5 +108,4 @@ public class Radio {
             currentVolume--;
         }
     }
-
 }
--- src/test/java/ru/netology/radioTest.java
+++ src/test/java/ru/netology/radioTest.java
@@ -20,12 +20,30 @@ class radioTest {
     }
 
     @Test
-    public void getNowParam() {
+    public void setNowParam() {
         Radio radio = new Radio(5, 73);
-        radio.getCurrentStation();
-        radio.getCurrentVolume();
         assertEquals(5, radio.getCurrentStation());
         assertEquals(73, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void setMaxParam() {
+        Radio radio = new Radio(9, 100);
+        assertEquals(9, radio.getCurrentStation());
+        assertEquals(100, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void setMinParam() {
+        Radio radio = new Radio(0, 0);
+        assertEquals(0, radio.getCurrentStation());
+        assertEquals(0, radio.getCurrentVolume());
+    }
+    @Test
+    public void setNegativeParam() {
+        Radio radio = new Radio(-1, -1);
+        assertEquals(0, radio.getCurrentStation());
+        assertEquals(0, radio.getCurrentVolume());
     }
 
     @Test
@@ -37,6 +55,23 @@ class radioTest {
         assertEquals(75, radio.getCurrentVolume());
     }
 
+    @Test
+    public void changeParamNowToNegative() {
+        Radio radio = new Radio(7, 50);
+        radio.setCurrentStation(-5);
+        radio.setCurrentVolume(-75);
+        assertEquals(0, radio.getCurrentStation());
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void changeParamNowToExceeded() {
+        Radio radio = new Radio(7, 50);
+        radio.setCurrentStation(15);
+        radio.setCurrentVolume(175);
+        assertEquals(9, radio.getCurrentStation());
+        assertEquals(100, radio.getCurrentVolume());
+    }
 
     @Test
     public void newParamBeforeMax() {
@@ -45,15 +80,6 @@ class radioTest {
         radio.increaseVolume();
         assertEquals(9, radio.getCurrentStation());
         assertEquals(100, radio.getCurrentVolume());
-    }
-
-    @Test
-    public void newParamBeforeMin() {
-        Radio radio = new Radio(-1, -1);
-        radio.next();
-        radio.increaseVolume();
-        assertEquals(0, radio.getCurrentStation());
-        assertEquals(0, radio.getCurrentVolume());
     }
 
     @Test
@@ -82,6 +108,89 @@ class radioTest {
         assertEquals(9, radio.getCurrentStation());
         assertEquals(0, radio.getCurrentVolume());
     }
+
+    @Test
+    public void countSattionNewParam() {
+        Radio radio = new Radio(25);
+        radio.setCurrentStation(15);
+        assertEquals(15, radio.getCurrentStation());
+    }
+
+    @Test
+    public void countSattionMaxStation() {
+        Radio radio = new Radio(190);
+        radio.setCurrentStation(189);
+        assertEquals(189, radio.getCurrentStation());
+    }
+
+    @Test
+    public void nextStationAfterMaxCountStation() {
+        Radio radio = new Radio(190);
+        radio.setCurrentStation(189);
+        radio.next();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void setNewWrongParam() {
+        Radio radio = new Radio(6, 56);
+        radio.setCurrentStation(15);
+        radio.setCurrentVolume(115);
+        assertEquals(9, radio.getCurrentStation());
+        assertEquals(100, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void setMaxCurrentStation() {
+        Radio radio = new Radio(10);
+        assertEquals(9, radio.getMaxCurrentStation());
+    }
+
+    @Test
+    public void setMinCurrentStation() {
+        Radio radio = new Radio(10);
+        assertEquals(0, radio.getMinCurrentStation());
+    }
+
+    @Test
+    public void setNewMaxWrongParam() {
+        Radio radio = new Radio(6, 56);
+        radio.setCurrentStation(15);
+        radio.setCurrentVolume(115);
+        assertEquals(9, radio.getCurrentStation());
+        assertEquals(100, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void setNewMimWrongParam() {
+        Radio radio = new Radio(6, 56);
+        radio.setCurrentStation(-10);
+        radio.setCurrentVolume(-110);
+        assertEquals(0, radio.getCurrentStation());
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void setNewMinusParam() {
+        Radio radio = new Radio(-6, -56);
+        assertEquals(0, radio.getMinCurrentStation());
+        assertEquals(0, radio.getMinCurrentVolume());
+    }
+
+    @Test
+    public void maxCurrentStation() {
+        Radio radio = new Radio();
+        assertEquals(9, radio.getMaxCurrentStation());
+    }
+
+    @Test
+    public void nextAfterMaxCurrentStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(radio.getMaxCurrentStation());
+        assertEquals(9, radio.getMaxCurrentStation());
+    }
 }
+
+
 
 
