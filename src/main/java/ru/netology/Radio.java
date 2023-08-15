2a3a6de7ea5a917394ed173c4b7cf248e34c3d51
Pavel Davydov <Davydov.works@gmail.com>
2023-08-15 00:12:07 -0400

    updated code + add Lombok

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,5 +1,12 @@
 package ru.netology;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Radio {
     private int minCurrentStation = 0;
     private int maxCurrentStation = 9;
@@ -12,39 +19,6 @@ public class Radio {
         currentStation = numRadio;
 
         currentVolume = sizeVolume;
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public int getMinCurrentStation() {
-        return minCurrentStation;
-    }
-
-    public int getMaxCurrentStation() {
-        return maxCurrentStation;
-    }
-
-    public int getMinCurrentVolume() {
-        return minCurrentVolume;
-    }
-
-    public int getMaxCurrentVolume() {
-        return maxCurrentVolume;
-    }
-
-
-    public void setCurrentStation(int newCurrentStation) {
-        currentStation = newCurrentStation;
-    }
-
-    public void setCurrentVolume(int newCurrentVolume) {
-        currentVolume = newCurrentVolume;
     }
 
     public void next() {
